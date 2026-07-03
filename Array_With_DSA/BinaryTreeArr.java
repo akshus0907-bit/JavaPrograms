90ee45682650a846cce6c47c898a943628906a77
Akshata Shinde <akshus0907@gmail.com>
2026-07-03 23:16:13 +0530

    Added Binary Tree Program

---
+++ Array_With_DSA/BinaryTreeArr.java
@@ -0,0 +1,54 @@
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+}
+
+public class BinaryTreeArr {
+
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return buildTree(nums, 0, nums.length - 1);
+    }
+
+    public TreeNode buildTree(int[] nums, int left, int right) {
+
+        if (left > right) {
+            return null;
+        }
+
+        int mid = left + (right - left) / 2;
+
+        TreeNode root = new TreeNode(nums[mid]);
+
+        root.left = buildTree(nums, left, mid - 1);
+        root.right = buildTree(nums, mid + 1, right);
+
+        return root;
+    }
+
+    // Inorder Traversal
+    public void inorder(TreeNode root) {
+        if (root == null)
+            return;
+
+        inorder(root.left);
+        System.out.print(root.val + " ");
+        inorder(root.right);
+    }
+
+    public static void main(String args[]) {
+
+        BinaryTreeArr obj = new BinaryTreeArr();
+
+        int nums[] = {-10, -3, 0, 5, 9};
+
+        TreeNode root = obj.sortedArrayToBST(nums);
+
+        System.out.println("Inorder Traversal:");
+        obj.inorder(root);
+    }
+}
